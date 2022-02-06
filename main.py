7eab10be2330f2d495ac74868d0369f2e3397b78
aigul <farkhetdinovaa1@mail.ru>
2022-02-06 20:14:19 +0300

    1, 2, 3, 4 task

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame
 import requests
+import math
 
 
 COORDS = [133.795393, -25.6947768]
@@ -10,6 +11,18 @@ map_api_server = "http://static-maps.yandex.ru/1.x/"
 map_file = "map.png"
 map_types = ['map', 'sat', 'sat,skl']
 type_ind = 0
+
+def change_ll(ind):
+    print('f')
+    if ind == 0:
+        COORDS[0] -= 3
+    elif ind == 1:
+        COORDS[0] += 3
+    elif ind == 2:
+        COORDS[1] += 3
+    else:
+        COORDS[1] -= 3
+    print(COORDS)
 
 
 def load_file(COORDS, SPN):
@@ -40,6 +53,8 @@ while running:
                 SPN = list(map(lambda x: str(float(x) - 0.5), SPN))
             elif event.key in [pygame.K_1, pygame.K_2, pygame.K_3]:
                 type_ind = [pygame.K_1, pygame.K_2, pygame.K_3].index(event.key)
+            elif event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
+                change_ll([pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN].index(event.key))
             load_file(COORDS, SPN)
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
