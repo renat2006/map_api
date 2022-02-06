54e1f5c67763f483bd58f3ed1b1a7e22035ecadc
aigul <farkhetdinovaa1@mail.ru>
2022-02-06 19:22:42 +0300

    2st task

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 import os
 import sys
-
 import pygame
 import requests
 
 
-ll = input("Введите координаты:").split()
-spn = input("Введите масштаб карты:").split()
+COORDS = [133.795393, -25.6947768]
+SPN = [1, 1]
 map_api_server = "http://static-maps.yandex.ru/1.x/"
-map_params = {
-    "ll": ",".join(ll),
-    "spn": ",".join(spn),
-    "l": "map"
-}
-response = requests.get(map_api_server, params=map_params)
-
-if not response:
-    print("Ошибка выполнения запроса")
-    sys.exit(1)
-
 map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+
+
+def load_file(COORDS, SPN):
+    map_params = {
+        "ll": ','.join(list(map(str, COORDS))),
+        "spn": ','.join(list(map(str, SPN))),
+        "l": "map"
+    }
+    response = requests.get(map_api_server, params=map_params)
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
+load_file(COORDS, SPN)
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                SPN = list(map(lambda x: str(float(x) + 0.5), SPN))
+            elif event.key == pygame.K_PAGEDOWN:
+                SPN = list(map(lambda x: str(float(x) - 0.5), SPN))
+            load_file(COORDS, SPN)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
 
-os.remove(map_file)
+pygame.quit()
