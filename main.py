62ae192062dc8d0767f254249f6b5aa173f59c70
aigul <farkhetdinovaa1@mail.ru>
2022-02-06 19:38:05 +0300

    1, 2, 4 task

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ COORDS = [133.795393, -25.6947768]
 SPN = [1, 1]
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 map_file = "map.png"
+map_types = ['map', 'sat', 'sat,skl']
+type_ind = 0
 
 
 def load_file(COORDS, SPN):
     map_params = {
         "ll": ','.join(list(map(str, COORDS))),
         "spn": ','.join(list(map(str, SPN))),
-        "l": "map"
+        "l": map_types[type_ind]
     }
     response = requests.get(map_api_server, params=map_params)
     with open(map_file, "wb") as file:
@@ -36,6 +38,8 @@ while running:
                 SPN = list(map(lambda x: str(float(x) + 0.5), SPN))
             elif event.key == pygame.K_PAGEDOWN:
                 SPN = list(map(lambda x: str(float(x) - 0.5), SPN))
+            elif event.key in [pygame.K_1, pygame.K_2, pygame.K_3]:
+                type_ind = [pygame.K_1, pygame.K_2, pygame.K_3].index(event.key)
             load_file(COORDS, SPN)
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
